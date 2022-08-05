645e0e868cd08ede29a7881b722065462bcc0a71
Dave Ochsenbein <david.ochsenbein@novartis.com>
2022-08-05 12:39:56 +0200

    infuriating regex troubles

--- word-count/word-count.jl
+++ word-count/word-count.jl
@@ -1,3 +1,29 @@
 function wordcount(sentence)
+    sentence = lowercase(sentence)
+    cleanedStr = replace(sentence, r"[^a-z0-9\s,']" => "")
+
+    # splitStr = split(cleanedStr)
+
+    splitStr = split(cleanedStr, (' ', ',', '\n'))
+
+    countDict = Dict()
+
+    for elem in splitStr
+
+        elem = strip(elem, ''')
+        print(elem)
+
+        if elem == "'" || elem == "," || elem == "\n" || elem == "\t" || elem == ""
+            continue
+        end
+
+        if haskey(countDict, elem)
+            countDict[elem] += 1
+        else
+            countDict[elem] = 1
+        end
+    end
+
+    return countDict
 
 end
